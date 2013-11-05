2b10bb54ce4ce229d6c84765ac70c56b6512cb0f
Néstor Álvarez Díaz <alu0100597943@ull.edu.es>
2013-11-05 16:23:48 +0000

    Segunda espectativa completada: Suma de matrices

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -6,32 +6,52 @@ class Matriz
 	
 	def initialize(fil,col=fil)
 		@fil, @col = fil, col
-		@matriz = Array.new(fil){Array.new(col){nil}}
+		@matriz = Array.new(fil){Array.new(col)}
 	end
 
 	def fill(array=nil)
 		if(array != nil)
-			for i in (0..@fil-1)
-				for j in (0..@col-1)
+			@fil.times do |i|
+				@col.times do |j|
 					@matriz[i][j] = array[i][j]
 				end
 			end
 		else
 			p_rand = Random.new()
-			for i in (0..@fil-1)
-				for j in (0..@col-1)
+			@fil.times do |i|
+				@col.times do |j|
 					@matriz[i][j] = p_rand.rand(30)
 				end
 			end
 		end
 	end
 	
+	def +(other)
+		if(other.is_a?Matriz)
+			result = Array.new(@fil){Array.new(@col) {0}}
+			@fil.times do |i|
+				@col.times do |j|
+	                result[i][j] = @matriz[i][j] + other.matriz[i][j]
+			    end
+			end
+			r = Matriz.new(@fil,@col)
+			r.fill(result)
+			r
+		end
+	end
+	
 	def ==(other)
 		cont = 0
-		for i in (0..@fil-1)
-			for j in (0..@col-1)
-				if(@matriz[i][j] == other[i][j])
-					cont += 1
+		@fil.times do |i|
+			@col.times do |j|
+				if(other.is_a?Matriz)
+					if(@matriz[i][j] == other.matriz[i][j])
+						cont += 1
+					end
+				else
+					if(@matriz[i][j] == other[i][j].to_i)
+						cont += 1
+					end
 				end
 			end
 		end
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -7,17 +7,14 @@ describe Matriz do
 	end
 	describe "# Almacenamiento" do
 		it "Se almacena correctamente" do
-			(@m1==([[1,2],[3,4]])).should eq(true)
+			(@m1 == ([[1,2],[3,4]])).should eq(true)
 		end
 	end
 	describe "# Operacion Suma" do
 		it "sumamos la matriz consigo misma" do
 			m = @m1+@m1
-			@m1[0].should eq(2)
-			@m1[1].should eq(4)
-			@m1[2].should eq(6)
-			@m1[3].should eq(8)
-			
+			(m == ([[2,4],[6,8]])).should eq(true)
+			#m.should eq([[2,4],[6,8]])
 		end
 	end
 	describe "# Multiplicacion" do
@@ -30,5 +27,4 @@ describe Matriz do
 
 		end
 	end
-	
 end
