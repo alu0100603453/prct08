f1b698f858b622f32ee005f2e56dfb16317790ac
Francisco Alderete <franalderete@gmail.com>
2013-11-05 11:10:17 +0000

    rakefile creado

---
+++ rakefile.rb
@@ -0,0 +1,11 @@
+$:.unshift File.dirname(__FILE__) + 'lib'
+$:.unshift './lib', './spec'
+
+require 'rspec/core/rake_task'
+RSpec::Core::RakeTask.new
+task :default => :spec
+
+desc "Ejecutar las espectativas de la clase Matrix"
+task :local do
+  sh "rspec --format documentation -Ilib -Ispec spec/matrix_spec.rb"
+end
